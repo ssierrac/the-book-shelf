16772725e331efc187690e08e283b7560fae3bab
ssierrac <sebastian.sierrajc@gmail.com>
2023-10-29 16:06:32 -0500

    book resource creation

---
+++ apps/books-service/src/books/books.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class BooksService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  findAll() {
+    return this.prisma.book.findMany({
+      where: { published: true },
+      include: { author: true },
+    });
+  }
+
+  findDrafts() {
+    return this.prisma.book.findMany({
+      where: { published: false },
+      include: { author: true },
+    });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
